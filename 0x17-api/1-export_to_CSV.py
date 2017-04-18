819536be649b38a71acea6ffd2fecba0b7ec3fae
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-18 02:13:04 +0000

    Indented correctly, pep8

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+GET username and TODOs from https://jsonplaceholder.typicode.com
+from employee ID and export data in CSV format
+"""
+import sys
+import requests
+import csv
+
+
+if __name__ == "__main__":
+    api = "https://jsonplaceholder.typicode.com/"
+    empId = sys.argv[1]
+    empName = requests.get("{}users/{}"
+                           .format(api, empId)).json().get("username")
+    empTodos = requests.get("{}users/{}/todos".format(api, empId)).json()
+
+    filename = "{}.csv".format(sys.argv[1])
+
+    with open("{}.csv".format(empId), mode="w") as csvFile:
+        writer = csv.writer(csvFile, delimiter=",", quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+
+        for toDo in empTodos:
+            holder = [empId, empName, toDo.get("completed"),
+                      toDo.get("title")]
+            writer.writerow(holder)
