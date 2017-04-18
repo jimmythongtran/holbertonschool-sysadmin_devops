379e3fab1116a08d616f55a06979f1c173b13f22
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-18 04:22:51 +0000

    Libraries alphabetical, pep8

--- 0x17-api/2-export_to_JSON.py
+++ 0x17-api/2-export_to_JSON.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+GET username and TODOs from https://jsonplaceholder.typicode.com
+from employee ID and export data in JSON format
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    api = "https://jsonplaceholder.typicode.com/"
+    empId = sys.argv[1]
+    empName = requests.get("{}users/{}"
+                           .format(api, empId)).json().get("username")
+    empTodos = requests.get("{}users/{}/todos".format(api, empId)).json()
+
+    todoList = []
+    for toDo in empTodos:
+        holder = {}
+        holder["completed"] = toDo.get("completed")
+        holder["toDo"] = toDo.get("title")
+        holder["username"] = empName
+        todoList.append(holder)
+
+    with open("{}.json".format(empId), mode="w") as jsonFile:
+        json.dump({empId: todoList}, jsonFile)
