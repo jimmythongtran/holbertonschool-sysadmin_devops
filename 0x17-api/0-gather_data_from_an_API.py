1af71e1864d51b1be88438b2ff330f4e656c7de1
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-18 00:09:15 +0000

    Pep8, finally working

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -1,4 +1,30 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+GET username and TODOs from https://jsonplaceholder.typicode.com
+from employee ID
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    api = "https://jsonplaceholder.typicode.com/"
+    empId = sys.argv[1]
+    empName = requests.get("{}users/{}".format(api, empId)).json().get("name")
+
+    totalTodos = 0
+    doneTodos = []
+
+    empTodos = requests.get("{}todos?userId={}".format(api, empId)).json()
+
+    for toDo in empTodos:
+        if toDo.get("userId") == int(empId):
+            totalTodos += 1
+            if (toDo.get("completed")):
+                doneTodos.append(toDo.get("title"))
+
+    print("Employee {:s} is done with tasks({:d}/{:d}):"
+          .format(empName, len(doneTodos), totalTodos))
+
+    for item in doneTodos:
+        print("\t{:s}".format(item))
