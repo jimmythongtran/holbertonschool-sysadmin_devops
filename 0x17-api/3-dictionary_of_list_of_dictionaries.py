5ced273bf151233e0f26c9022af4f6b00565ce92
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-04-18 20:32:15 +0000

    pep8, works

--- 0x17-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x17-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,4 +1,30 @@
 #!/usr/bin/python3
-REPLACE
-    return (0)
-print(REPLACE)
+"""
+GET username and TODOs from https://jsonplaceholder.typicode.com
+from employee ID and export data in JSON format for all employees
+"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    api = "https://jsonplaceholder.typicode.com/"
+    allUsers = requests.get("{}users".format(api)).json()
+    empTodos = requests.get("{}todos".format(api)).json()
+
+    records = {}
+    for user in allUsers:
+        empId = user.get("id")
+        empName = user.get("username")
+        todoList = []
+
+    for toDo in empTodos:
+        holder = {}
+        holder["completed"] = toDo.get("completed")
+        holder["toDo"] = toDo.get("title")
+        holder["username"] = empName
+        todoList.append(holder)
+
+    records[empId] = todoList
+    with open("todo_all_employees.json", mode="w") as jsonFile:
+        json.dump(records, jsonFile)
